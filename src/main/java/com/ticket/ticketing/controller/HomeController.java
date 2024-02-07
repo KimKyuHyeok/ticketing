bdce2f0c42ead1744620046c18b3fb5f7f37fe1b
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-02-07 17:57:34 +0900

    신규로 등록된 콘서트들을 html 에 model 을 통해서 작성할 수 있게 변경

--- src/main/java/com/ticket/ticketing/controller/HomeController.java
+++ src/main/java/com/ticket/ticketing/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.ticket.ticketing.controller;
 
+import com.ticket.ticketing.dto.ConcertDto;
+import com.ticket.ticketing.service.ConcertService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+    private final ConcertService concertService;
 
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+
+        List<ConcertDto> concertDtoList = concertService.getNewConcertList();
+
+        model.addAttribute("concertList", concertDtoList);
+
         return "home";
     }
 }
