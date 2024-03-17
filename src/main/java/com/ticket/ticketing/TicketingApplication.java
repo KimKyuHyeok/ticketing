4a0ee60ead646f6443e7d151ebaa2f47ca5c603e
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-03-18 01:41:19 +0900

    AWS sdk 에러 수정

--- src/main/java/com/ticket/ticketing/TicketingApplication.java
+++ src/main/java/com/ticket/ticketing/TicketingApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class TicketingApplication {
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingApplication.class, args);
